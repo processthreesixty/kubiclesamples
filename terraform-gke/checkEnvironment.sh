bb0e45a7eae668b3cceb88ac69282e5475035259
raziqtechnologies <81863529+raziqtechnologies@users.noreply.github.com>
2024-12-13 14:33:40 +0000

    add the environment

--- terraform-gke/checkEnvironment.sh
+++ terraform-gke/checkEnvironment.sh
@@ -1,2 +1,3 @@
 export GOOGLE_APPLICATION_CREDENTIALS=/home/kubicle/.config/gcloud/application_default_credentials.json
 echo $GOOGLE_APPLICATION_CREDENTIALS
+gcloud auth application-default print-access-token
