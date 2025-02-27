f794d5a5b7a810a6d7ae779fa92729485a66ec55
unknown <ahmed.mustaq@gmail.com>
2025-02-27 11:52:06 +0000

    new wordpress terraform added

--- bash/wordpress.sh
+++ bash/wordpress.sh
@@ -1,1 +1,3 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
 helm install my-wordpress bitnami/wordpress --set wordpressPassword=my-password --set mysqlRootPassword=my-root-password --set mysqlPassword=my-mysql-password
